0997dd3f82ed1cc859dc4c7f70597d494463ed83
Devarsh-AD <xsantosh7@gmail.com>
2023-01-12 10:49:13 +0530

    added deleteData & updateData method to update & delete from firebase cloud

--- lib/cloud_fireStore/create_profile.dart
+++ lib/cloud_fireStore/create_profile.dart
@@ -3,6 +3,7 @@
 import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class CreateProfile extends StatelessWidget {
@@ -23,6 +24,9 @@ class SubmitForm extends StatefulWidget {
 
 class _SubmitFormState extends State<SubmitForm> {
   final nameController = TextEditingController();
+  final ageController = TextEditingController();
+  final birthdateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,12 +50,54 @@ class _SubmitFormState extends State<SubmitForm> {
                         OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0)))),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: ageController,
+                // keyboardType: TextInputType.,
+                decoration: const InputDecoration(
+                    floatingLabelAlignment: FloatingLabelAlignment.center,
+                    labelText: 'age',
+                    border:
+                        OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: birthdateController,
+                // keyboardType: TextInputType.,
+                decoration: const InputDecoration(
+                    floatingLabelAlignment: FloatingLabelAlignment.center,
+                    labelText: 'birth',
+                    border:
+                        OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+              ),
+            ),
             ElevatedButton(
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.green.shade600),
                 onPressed: () async {
-                  await createUser(name: nameController.text.trim());
+                  await createUser(
+                      name: nameController.text.trim(),
+                      age: int.parse(ageController.text.trim()),
+                      birth: ageController.text.trim());
                 },
                 child: Text('Submit')),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.green.shade600),
+                onPressed: () async {
+                  updateData(
+                      age: int.parse(ageController.text.trim()),
+                      birth: ageController.text.trim(),
+                      name: nameController.text.trim());
+                },
+                child: Text('Update Data')),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.green.shade600),
+                onPressed: () async {
+                  await deleteData();
+                },
+                child: Text('Delete Data')),
             StreamBuilder(
                 stream: readData(),
                 builder: (context, snapshoot) {
@@ -61,7 +107,7 @@ class _SubmitFormState extends State<SubmitForm> {
                       children: [
                         Text('age ${snapshoot.data!.map((e) => e.age)}'),
                         Text('name ${snapshoot.data!.map((e) => e.name)}'),
-                        Text('bithdate ${snapshoot.data!.map((e) => e.birthdate)}'),
+                        Text('bithdate ${snapshoot.data!.map((e) => e.birthdate.toString())}'),
                       ],
                     );
                   } else {
@@ -74,11 +120,11 @@ class _SubmitFormState extends State<SubmitForm> {
     );
   }
 
-  Future<void> createUser({required String name}) async {
+  Future<void> createUser({required String name, required int age, required String birth}) async {
     // if you remove id from [doc] then firebase will generate id automatically
     final docUser = FirebaseFirestore.instance.collection('dev').doc('fsf');
     // insted of json you can make model class with to json method
-    final json = {'name': name, 'age': 21, 'birth': DateTime(2000, 5, 22)};
+    final json = {'name': name, 'age': age, 'birth': birth};
     final user = UserData(age: 21, birthdate: '23-3-2002', name: name);
     final json2 = user.toJson();
 
@@ -87,10 +133,38 @@ class _SubmitFormState extends State<SubmitForm> {
   }
 }
 
+updateData({required String name, required int age, required String birth}) {
+  final updateData = FirebaseFirestore.instance.collection('dev').doc('fsf');
+  final user = UserData(age: age, birthdate: birth, name: name);
+  final jsonData = user.toJson();
+  updateData.update(jsonData);
+}
+
 Stream<List<UserData>> readData() => FirebaseFirestore.instance
     .collection('dev')
     .snapshots()
     .map((snapShoot) => snapShoot.docs.map((doc) => UserData.fromJson(doc.data())).toList());
+
+deleteData() {
+  final updateData = FirebaseFirestore.instance.collection('dev').doc('fsf');
+  //To access nested valued user [.] oprator in key of that like
+  /* 
+            name : {
+              firstName: 'abc',
+              lastNmae : 'xyz'
+            },
+            age : 22,
+            birth: 1-1-2001
+            
+            => to delete nested field
+            updateData.update({'name.firstName': FieldValue.delete()});
+
+            => to update nested field
+            updateData.update({'name.firstName': 'pqr'});
+
+  */
+  updateData.update({'name': FieldValue.delete()});
+}
 
 class UserData {
   int age;
